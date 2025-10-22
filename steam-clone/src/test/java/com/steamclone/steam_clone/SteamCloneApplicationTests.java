809711eaef692b4a1355cdeec147ce15bf431e87
Keeeichiii <renassannn@gmail.com>
2025-10-22 03:30:16 +0300

    Adding Spring Security and Testing the Database Connection

--- steam-clone/src/test/java/com/steamclone/steam_clone/SteamCloneApplicationTests.java
+++ steam-clone/src/test/java/com/steamclone/steam_clone/SteamCloneApplicationTests.java
@@ -1,13 +1,25 @@
 package com.steamclone.steam_clone;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
 class SteamCloneApplicationTests {
 
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
 	@Test
 	void contextLoads() {
 	}
+
+    @Test
+    void testDatabaseConnection() {
+        assertThat(jdbcTemplate).isNotNull();
+    }
 
 }
